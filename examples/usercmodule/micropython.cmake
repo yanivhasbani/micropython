be4e287d950021fb8cdf623e86582e6f3af43d30
yanivhasbani <yanivhasbanidev@gmail.com>
2021-10-04 14:58:47 +0300

    Removed CPP user

--- examples/usercmodule/micropython.cmake
+++ examples/usercmodule/micropython.cmake
@@ -6,6 +6,3 @@
 # Add the C example.
 include(${CMAKE_CURRENT_LIST_DIR}/cexample/micropython.cmake)
 
-# Add the CPP example.
-include(${CMAKE_CURRENT_LIST_DIR}/cppexample/micropython.cmake)
-
--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-codesign -f --deep -s "-" ../ports/unix/micropython
-scp -p ../ports/unix/micropython root@localhost:/private/var/db/com.apple.xpc.roleaccountd.staging/
-ssh -p 22 root@localhost
-cd /private/var/db/com.apple.xpc.roleaccountd.staging/
